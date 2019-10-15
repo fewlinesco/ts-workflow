f8216daf03f6fa317553420fc38030b218bdc97d
Julien Leclercq <julien.leclercq@gmx.fr>
2019-10-15 15:16:23 +0200

    better use of export statement

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,8 @@
-import {
+export {
   Logger,
   Payload,
   UnexpectedError,
   Workflow,
-  makePayload as workflowMakePayload,
-  makeWorkflow as workflowMakeWorkflow
+  makePayload,
+  makeWorkflow
 } from "./workflow";
-
-export type Logger = Logger;
-export type Payload<Input> = Payload<Input>;
-export type UnexpectedError = UnexpectedError;
-export type Workflow<Input, Result> = Workflow<Input, Result>;
-
-export const makePayload = workflowMakePayload;
-export const makeWorkflow = workflowMakePayload;
